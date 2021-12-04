d26dd69468258f6dffe4a810a6dec230e35c1ecd
Shokochoco <shsuzu1992@gmail.com>
2021-12-04 21:18:43 +0900

    First Commit

--- Kadai-6/ViewController.swift
+++ Kadai-6/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
+//enum AlertMessage: String {
+//    case correct
+//    case incorrect
+//
+//    var message: String {
+//        switch self {
+//        case .correct:
+//            return "あたり！"
+//        case .incorrect:
+//            return "はずれ！"
+//        }
+//    }
+//}
+
 class ViewController: UIViewController {
+    @IBOutlet private weak var numberLabel: UILabel!
+    @IBOutlet private weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createRandomNumber()
     }
 
+    @IBAction private func judgeButtonTapped(_ sender: Any) {
+        let sliderValue = Int(slider.value)
+        let sliderNumString = String(sliderValue)
+        if numberLabel.text == sliderNumString {
+            actionAlert(message: "あたり！", selectedNum: sliderNumString)
+        } else {
+            actionAlert(message: "はずれ！", selectedNum: sliderNumString)
+        }
+    }
 
+    func createRandomNumber() {
+        let randomNum = Int.random(in: 1...100)
+        numberLabel.text = String(randomNum)
+    }
+
+    func actionAlert(message: String, selectedNum: String) {
+        let alert = UIAlertController(title: "結果", message: "\(message)　　　　　　　　　　　　　　　　　　　　　　　　　あなたの値：\(selectedNum)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "再挑戦", style: .default, handler: { _ in
+            self.createRandomNumber()
+        }))
+        present(alert, animated: true, completion: nil)
+    }
 }
-
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Kadai-6' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Kadai-6
+    pod 'SwiftLint'
+
+end
